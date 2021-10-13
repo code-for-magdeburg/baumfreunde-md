ac10bc270fe4a431a92554db0df88856515b6867
Jens Winter <jens.winter@gmail.com>
2021-10-13 09:14:39 +0200

    Compress data files during CI

---
+++ scripts/brotli-compress.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const brotli = require('brotli');
+
+
+const brotliSettings = {
+  extension: 'br',
+  skipLarger: true,
+  mode: 1,
+  quality: 10,
+  lgwin: 12,
+  threshold: 10240
+};
+
+const DATA_PATH = './dist/baumfreunde-md-web/assets/data/';
+
+
+fs.readdirSync(DATA_PATH).forEach(file => {
+
+  if (file.endsWith('.csv')) {
+    const result = brotli.compress(fs.readFileSync(DATA_PATH + file), brotliSettings);
+    fs.writeFileSync(`${DATA_PATH}${file}.br`, result);
+  }
+
+});
