82a355acb4f83ca479b3a329387b13a3271d1c26
Jens Winter <jens.winter@gmail.com>
2021-10-14 21:29:18 +0200

    feat: replace EasyButton with standard BS button
    Refs: #31

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { RegularTreeDetailsComponent } from './regular-tree-details/regular-tree-details.component';
 import 'leaflet-easybutton';
 import { SearchTreeDialogComponent } from './search-tree-dialog/search-tree-dialog.component';
-import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCrosshairs, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Offcanvas } from 'bootstrap';
 import { DataService } from '../services/data.service';
 import { TreeDataPoint } from '../model/TreeDataPoint';
@@ -27,6 +27,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   faBars = faBars;
   faSearch = faSearch;
+  faCrosshairs = faCrosshairs;
 
   dataIsLoading = false;
 
@@ -80,26 +81,9 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
 
   mapReady(map: L.Map): void {
-
     map.attributionControl.setPosition('topright');
-
     map.addControl(L.control.zoom({ position: 'topright' }));
-
-    const jumpToCurrentLocationButton = L.easyButton({
-      position: 'topright',
-      states: [
-        {
-          stateName: 'default',
-          title: 'Zur aktuellen Position',
-          icon: '<svg aria-hidden="true" width="20" focusable="false" data-prefix="fas" data-icon="crosshairs" class="svg-inline--fa fa-crosshairs fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M500 224h-30.364C455.724 130.325 381.675 56.276 288 42.364V12c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v30.364C130.325 56.276 56.276 130.325 42.364 224H12c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h30.364C56.276 381.675 130.325 455.724 224 469.636V500c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-30.364C381.675 455.724 455.724 381.675 469.636 288H500c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12zM288 404.634V364c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v40.634C165.826 392.232 119.783 346.243 107.366 288H148c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12h-40.634C119.768 165.826 165.757 119.783 224 107.366V148c0 6.627 5.373 12 12 12h40c6.627 0 12-5.373 12-12v-40.634C346.174 119.768 392.217 165.757 404.634 224H364c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h40.634C392.232 346.174 346.243 392.217 288 404.634zM288 256c0 17.673-14.327 32-32 32s-32-14.327-32-32c0-17.673 14.327-32 32-32s32 14.327 32 32z"></path></svg>',
-          onClick: () => this.jumpToCurrentLocation()
-        }
-      ]
-    });
-    map.addControl(jumpToCurrentLocationButton);
-
     this.map = map;
-
   }
 
 
@@ -114,7 +98,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
 
-  private jumpToCurrentLocation(): void {
+  jumpToCurrentLocation(): void {
     navigator.geolocation.getCurrentPosition(
       resp => this.jumpToLocation(resp.coords.latitude, resp.coords.longitude),
       positionError => console.log(positionError));
