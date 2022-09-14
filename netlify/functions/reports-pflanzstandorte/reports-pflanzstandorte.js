2aee3fb2b5931805b10dc4a96b082a17488e9d9d
Jens Winter <jens.winter@gmail.com>
2022-09-14 06:42:04 +0200

    add function reports-nachpflanzungen

---
+++ netlify/functions/reports-pflanzstandorte/reports-pflanzstandorte.js
@@ -0,0 +1,40 @@
+
+const { MongoClient, ObjectId } = require('mongodb');
+const moment = require('moment');
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+const clientPromise = mongoClient.connect();
+
+
+const handler = async (event) => {
+
+  try {
+
+    const db = (await clientPromise).db(process.env.DATABASE_NAME);
+
+    const { reportId } = event.queryStringParameters;
+    if (reportId) {
+
+      const report = await db.collection('report-pflanzstandorte').findOne({ _id: ObjectId(reportId) });
+
+      return { statusCode: 200, body: JSON.stringify(report) };
+
+    } else {
+
+      const year = event.queryStringParameters.year || moment().format('YYYY');
+      const reports = await db
+        .collection('report-pflanzstandorte')
+        .find({ reportedDate: { $gte: moment.utc(`${year}-01-01`).toDate(), $lt: moment.utc(`${year}-01-01`).add(1, 'year').toDate() }})
+        .toArray();
+
+      return { statusCode: 200, body: JSON.stringify(reports) };
+
+    }
+
+  }  catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+
+}
+
+module.exports = { handler }
