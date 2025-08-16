6fa8369d7a96148df8bf020e3b9ca9298254d8f8
Jens Winter-Huebenthal <jens.winter-huebenthal@bridgefield.de>
2025-08-16 18:52:41 +0200

    update ngx-leaflet, angular-fontawesome, ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
 import { HomeComponent } from './home/home.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ModalModule } from 'ngx-bootstrap/modal';
